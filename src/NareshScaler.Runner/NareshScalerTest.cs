1d235fbb1f0bcc350a4acf2b80b57763108baaaa
Iain Hunter <hunter_iain@yahoo.co.uk>
2012-03-29 22:10:08 +0100

    Add NareshScaler source

---
+++ src/NareshScaler.Runner/NareshScalerTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace NareshScaler.Runner
+{
+
+    public abstract class NareshScalerTest
+    {
+        public FirefoxDriver FirefoxDriver;
+        public InternetExplorerDriver IEDriver;
+        public ChromeDriver ChromeDriver;
+
+
+        public TimeSpan DefaultTimeOutValue;
+
+        protected NareshScalerTest()
+        {
+            // Set default timeout to 5s
+            DefaultTimeOutValue = new TimeSpan(0, 0, 10);
+        }
+
+
+        /// <summary>
+        /// Runs test using ChromeDriver
+        /// </summary>
+        [Test]
+        public virtual void ChromeTest()
+        {
+            //Set the lib dir for the running solution
+            var currentDir = Directory.GetCurrentDirectory();
+
+            var packagesDir = LocateDir(currentDir, "packages");
+
+            // TODO - Should pick up build number from Assembly
+            var driverDir = new DirectoryInfo(packagesDir).FullName + "\\NareshScaler.1.0.0.12\\bin\\";
+
+            try
+            {
+                ChromeDriver = new ChromeDriver(driverDir);
+            }
+            catch (Exception)
+            {
+                // Only for master build
+                var masterLibDir = LocateDir(currentDir, "lib");
+                ChromeDriver = new ChromeDriver(masterLibDir);
+            }
+            
+            ChromeDriver.Manage().Timeouts().ImplicitlyWait(DefaultTimeOutValue);
+
+            // Moved out of try catch block to trigger error on build server.
+            RunSeleniumTests(ChromeDriver);
+        }
+
+        /// <summary>
+        /// Runs the test using the Firefox driver
+        /// </summary>
+        [Test]
+        public virtual void FirefoxTest()
+        {
+            FirefoxDriver = new FirefoxDriver();
+            FirefoxDriver.Manage().Timeouts().ImplicitlyWait(DefaultTimeOutValue);
+
+            RunSeleniumTests(FirefoxDriver);
+        }
+
+        /// <summary>
+        /// Runs the test using the IE driver
+        /// </summary>
+        [Test]
+        public virtual void IETest()
+        {
+            IEDriver = new InternetExplorerDriver();
+            IEDriver.Manage().Timeouts().ImplicitlyWait(DefaultTimeOutValue);
+            RunSeleniumTests(IEDriver);
+        }
+
+        /// <summary>
+        /// Helper method to look discover where the ChromeDriver executeable lives
+        /// </summary>
+        private static string LocateDir(string currentDir, string dirToFind)
+        {
+            // Locate the packages dir in the running solution
+            if (currentDir.ToLower().Contains(dirToFind))
+                return currentDir;
+
+            var dirPath = new DirectoryInfo(currentDir).Parent.FullName;
+            string[] dirs = Directory.GetDirectories(dirPath);
+
+            foreach (string dir in dirs)
+            {
+                if (dir.ToLower().Contains(dirToFind))
+                    return dir;
+            }
+
+            // recurse up the tree until we find the packages dir
+            return LocateDir(dirPath, dirToFind);
+        }
+
+        /// <summary>
+        /// Override this method and use it to run any selenium methods created by the
+        /// Selenium Webdriver.
+        /// </summary>
+        /// <param name="webDriver"></param>
+        public abstract void RunSeleniumTests(IWebDriver webDriver);
+    }
+}
---
+++ src/NareshScaler.Runner/Samples/NugetOrgTest.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace NareshScaler.Runner.Samples
+{
+    [TestFixture]
+    public class NugetOrgTest : NareshScalerTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            baseURL = "http://nuget.org/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        public void Test_That_NareshScaler_Exists_On_NugetOrg()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/");
+            driver.FindElement(By.Id("searchBoxInput")).Clear();
+            driver.FindElement(By.Id("searchBoxInput")).SendKeys("NareshScaler");
+            driver.FindElement(By.Id("searchBoxSubmit")).Click();
+            driver.FindElement(By.LinkText("Naresh Scaler")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public override void RunSeleniumTests(IWebDriver webDriver)
+        {
+            driver = webDriver;
+            Test_That_NareshScaler_Exists_On_NugetOrg();
+        }
+    }
+}
